b55291ae85ed0dec6a383d6d20969a272817e7bc
JInWoong Hong <slsl03264@naver.com>
2024-09-03 15:06:24 +0900

    초기 세팅

---
+++ src/main/java/com/example/ShareFaith/ShareFaithApplication.java
@@ -0,0 +1,13 @@
+package com.example.ShareFaith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareFaithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareFaithApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ShareFaith/ShareFaithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ShareFaith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareFaithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
